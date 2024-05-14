7823116445a319d721b3eaf9a252612461c3dfc6
mibeit0815 <michi.becker2000@gmx.de>
2024-05-14 18:36:13 +0200

    1

--- Zeiterfassung/script.js
+++ Zeiterfassung/script.js
@@ -5,7 +5,8 @@ const path = require('path');
 const app = express();
 const PORT = 3030;
 const DATA_FILE = path.join(__dirname, 'data', 'timeRecords.json');
-const xlsx = require('xlsx');   
+const xlsx = require('xlsx');  
+const session = require('express-session'); 
 // Ensure data directory exists
 const dataDir = path.join(__dirname, 'data');
 if (!fs.existsSync(dataDir)) {
@@ -36,10 +37,18 @@ app.get('/timeRecords', (req, res) => {
     res.json(timeRecords);
 });
 
+app.use(session({
+    secret: 'hallowiegehtsdir',
+    resave: false,
+    saveUninitialized: true
+}));
+
+
 // Add new time record
 app.post('/timeRecords', (req, res) => {
   const newTimeRecord = req.body;
   newTimeRecord.id = Date.now(); // Assign a unique ID
+  newTimeRecord.user = req.session.user.username; 
   timeRecords.push(newTimeRecord);
   saveDataToFile(timeRecords);
   console.log("Time record added for: "+newTimeRecord.start)
@@ -59,6 +68,7 @@ app.delete('/timeRecords/:id', (req, res) => {
         res.status(404).send();
     }
 });
+
 
 // Function to save data to file
 function saveDataToFile(data) {
@@ -103,6 +113,7 @@ app.post('/login', (req, res) => {
     for (let row of data) {
         if (row.username === username && Number(row.password) === Number(password)) {
             validCredentials = true;
+            req.session.user = row;
             break;
         }
     }
