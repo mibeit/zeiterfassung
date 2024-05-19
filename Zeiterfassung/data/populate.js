7d4ab0213156cd5df57336963fb78e878bb71b92
mibeit0815 <michi.becker2000@gmx.de>
2024-05-19 13:06:09 +0200

    123

--- Zeiterfassung/data/populate.js
+++ Zeiterfassung/data/populate.js
@@ -1,4 +1,7 @@
 const fs = require('fs');
+
+let idCounter = 0;
+
 function generateDataEntries(startUser, startTime, endTime) {
     let data = [];
     let currentDate = new Date(); 
@@ -11,35 +14,44 @@ function generateDataEntries(startUser, startTime, endTime) {
                 "start": startTime,
                 "end": endTime,
                 "date": `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`,
-                "id": Date.now() - currentDate.getTime(), 
+                "id": idCounter++, 
                 "user": startUser
             };
 
             data.push(entry);
         }
 
-       
         currentDate.setDate(currentDate.getDate() - 1);
     }
 
     return data;
 }
-// hier dann ändern wer und wie lange 
-let newData = generateDataEntries("Clara", "6:00", "14:30");
 
+
+let users = [
+    { name: "Deborah", start: "6:00", end: "14:30" },
+    { name: "Luisa", start: "7:00", end: "15:30" },
+    { name: "Clara", start: "8:00", end: "16:30" },
+    { name: "Prisca", start: "14:00", end: "22:30" },
+    { name: "Aitana", start: "18:00", end: "23:30" },
+    { name: "Vivien", start: "2:00", end: "7:30" },
+    { name: "Elena", start: "10:00", end: "15:30" }
+   
+];
 
 fs.readFile('timeRecords.json', 'utf8', (err, data) => {
     if (err) throw err;
 
     let existingData = JSON.parse(data);
 
-    
-    let combinedData = existingData.concat(newData);
+    users.forEach(user => {
+        let newData = generateDataEntries(user.name, user.start, user.end);
+        existingData = existingData.concat(newData);
+    });
 
-    combinedData.reverse();
+    existingData.reverse();
 
-    
-    fs.writeFile('timeRecords.json', JSON.stringify(combinedData, null, 2), (err) => {
+    fs.writeFile('timeRecords.json', JSON.stringify(existingData, null, 2), (err) => {
         if (err) throw err;
         console.log('Data written to file');
     });
